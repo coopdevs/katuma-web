52ef47a10e161d1413e463e7453e644a31a3f23c
Andres Gutierrez <andres@redbooth.com>
2015-10-02 20:21:38 +0200

    WIP

--- src/redux/modules/auth.js
+++ src/redux/modules/auth.js
@@ -42,7 +42,7 @@ export default function reducer(state = initialState, action = {}) {
       return {
         ...state,
         loggingIn: false,
-        user: action.result.user
+        user: action.result
       };
     case LOGIN_FAIL:
       return {
