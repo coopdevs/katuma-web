8362e161009c5c12ddb1448639cd1bcd9eff0047
Andres Gutierrez <andres@redbooth.com>
2015-10-15 12:05:44 +0200

    Catch and display errors during React render

--- webpack/dev.config.js
+++ webpack/dev.config.js
@@ -1,3 +1,5 @@
+require('babel-core/polyfill');
+
 var fs = require('fs');
 var path = require('path');
 var webpack = require('webpack');
@@ -11,25 +13,38 @@ var WebpackIsomorphicToolsPlugin = require('webpack-isomorphic-tools/plugin');
 var webpackIsomorphicToolsPlugin = new WebpackIsomorphicToolsPlugin(require('./webpack-isomorphic-tools'));
 
 var babelrc = fs.readFileSync('./.babelrc');
-var babelLoaderQuery = {};
+var babelrcObject = {};
 
 try {
-  babelLoaderQuery = JSON.parse(babelrc);
+  babelrcObject = JSON.parse(babelrc);
 } catch (err) {
   console.error('==>     ERROR: Error parsing your .babelrc.');
   console.error(err);
 }
 
+var babelrcObjectDevelopment = babelrcObject.env && babelrcObject.env.development || {};
+var babelLoaderQuery = Object.assign({}, babelrcObject, babelrcObjectDevelopment);
+delete babelLoaderQuery.env;
+
 babelLoaderQuery.plugins = babelLoaderQuery.plugins || [];
-babelLoaderQuery.plugins.push('react-transform');
+if (babelLoaderQuery.plugins.indexOf('react-transform') < 0) {
+  babelLoaderQuery.plugins.push('react-transform');
+}
+
 babelLoaderQuery.extra = babelLoaderQuery.extra || {};
-babelLoaderQuery.extra['react-transform'] = {
-  transforms: [{
-    transform: 'react-transform-hmr',
-    imports: ['react'],
-    locals: ['module']
-  }]
-};
+if (!babelLoaderQuery.extra['react-transform']) {
+  babelLoaderQuery.extra['react-transform'] = {};
+}
+
+if (!babelLoaderQuery.extra['react-transform'].transforms) {
+  babelLoaderQuery.extra['react-transform'].transforms = [];
+}
+
+babelLoaderQuery.extra['react-transform'].transforms.push({
+  transform: 'react-transform-hmr',
+  imports: ['react'],
+  locals: ['module']
+});
 
 module.exports = {
   devtool: 'inline-source-map',
