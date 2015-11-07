58020d4f6e34f38912e2752efa769e625d39b695
Andres Gutierrez <andres@redbooth.com>
2015-11-07 10:39:25 +0100

    fix mobile rendering

--- src/helpers/Html.js
+++ src/helpers/Html.js
@@ -30,6 +30,8 @@ export default class Html extends Component {
 
           <link rel="shortcut icon" href="/favicon.ico" />
 
+          <meta name="viewport" content="width=device-width, initial-scale=1" />          
+
           {/* styles (will be present only in production with webpack extract text plugin) */}
           {Object.keys(assets.styles).map((style, i) =>
             <link href={assets.styles[style]} key={i} media="screen, projection"
