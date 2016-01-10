f755a536842da50d615d1d6921ddf9e8348f6218
Andres Gutierrez <andres@redbooth.com>
2016-01-10 16:49:36 +0100

    Make stateless components simple functions

--- src/containers/NotFound/NotFound.js
+++ src/containers/NotFound/NotFound.js
@@ -1,12 +1,10 @@
-import React, {Component} from 'react';
+import React from 'react';
 
-export default class NotFound extends Component {
-  render() {
-    return (
+export default function NotFound() {
+  return (
       <div className="container">
-        <h1>Doh! 404!</h1>
-        <p>These are <em>not</em> the droids you are looking for!</p>
+      <h1>Doh! 404!</h1>
+      <p>These are <em>not</em> the droids you are looking for!</p>
       </div>
-    );
-  }
+  );
 }
