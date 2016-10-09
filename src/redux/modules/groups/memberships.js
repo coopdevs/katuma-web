08a716ac92d6c1033914ad70cba51bcba1838bf4
Andres Gutierrez <andres@redbooth.com>
2016-10-09 12:08:55 +0200

    Fix default state for memberships reducer

--- src/redux/modules/groups/memberships.js
+++ src/redux/modules/groups/memberships.js
@@ -4,7 +4,11 @@ const LOAD_SUCCESS = 'redux-example/memberships/LOAD_SUCCESS';
 const LOAD_FAIL = 'redux-example/memberships/LOAD_FAIL';
 
 const initialState = {
-  memberships: {entities: [], byUserID: []}
+  memberships: {
+    entities: [],
+    byUserID: [],
+    byBasicResourceGroupId: {},
+  },
 };
 
 export default function membershipsReducer(state = initialState, action = {}) {
